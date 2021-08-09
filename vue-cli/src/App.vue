555229d3882cc274e874e3b02a38d161867ed84e
YunHak <sza0203@naver.com>
2021-08-09 23:39:38 +0900

    싱글 파일 컴포넌트에서 props 속성사용하기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//컴포넌트 폴더에 있는 앱해더 임포트 하기
+import AppHeader from './components/AppHeader.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data: function(){
+    return {
+      str: 'Header'
+    }
+  },
+  components:{
+    'app-header' : AppHeader
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,22 @@
+var appHeader = {
+    template: '<div>header</div>'
+}
+
+<template>
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    // javascript - 인스턴스 옵션
+    methods: {
+        addNum: function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
